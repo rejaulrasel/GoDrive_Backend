4df7ae4cdd0c4679d8d870f85757c05e4ecb6f09
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-09-22 01:21:32 +0600

    added interface and errors

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -6,4 +6,8 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUND,
+  jwt_access_token: process.env.JWT_ACCESS_TOKEN,
+  jwt_refresh_token: process.env.JWT_REFRESH_TOKEN,
+  stripe_secret: process.env.STRIPE_SECRET
 };
