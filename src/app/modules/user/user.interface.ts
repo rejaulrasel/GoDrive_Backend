6bd1b3c4b475c4dd8b961cc79e9562e3971c6840
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-09-22 01:32:28 +0600

    initialize user modules and added user interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,14 @@
+export interface TUser {
+    name: string;
+    photo: string;
+    email: string;
+    role: 'user' | 'admin';
+    password: string;
+    phone: string;
+    address: string;
+};
+
+export interface TLogin {
+    email: string;
+    password: string;
+}
